ce93a89c221ff00caa900fddec09418abf9b43e2
msoh <journy002@naver.com>
2022-01-02 13:34:22 -0800

    update contents

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,49 @@
+import React, { Component } from "react";
 import "./App.css";
 import Habits from "./components/habits";
 
-function App() {
-  return (
-    <div>
-      <Habits />
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: "Reading", count: 0 },
+      { id: 2, name: "Running", count: 0 },
+      { id: 3, name: "Coding", count: 0 },
+    ],
+  };
 
+  mainIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    console.log(habit, "habit");
+    console.log(index, "increment");
+    habits[index].count++;
+    this.setState({ habits: habits });
+  };
+
+  mainDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count > 0 ? count : 0;
+    this.setState({ habits });
+  };
+
+  mainDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+
+  render() {
+    return (
+      <div>
+        <Habits
+          habits={this.state.habits}
+          handleIncrement={this.mainIncrement}
+          handleDecrement={this.mainDecrement}
+          handleDelete={this.mainDelete}
+        />
+      </div>
+    );
+  }
+}
 export default App;
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -2,44 +2,33 @@ import React, { Component } from "react";
 import Habit from "./habit";
 
 class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: "Reading", count: 0 },
-      { id: 2, name: "Running", count: 0 },
-      { id: 3, name: "Coding", count: 0 },
-    ],
+  // this.props.handleIncrement() 괄호 안에 this.props.habits를 넣어 주었는데 에러가 떴다.
+  // 에러가 난 이유는 this.props.habits를 map으로 돌리면 요소를 하나씩 하나씩 돌기 때문에
+  // 요소를 habit={item}으로 하나씩 전달해서 요소의 데이터를 사용하면 되지만
+  // this.props.handleIncrement()는 그렇지 않기 때문에 인자값을 넣어줘서
+  // 정확히 어떤 요소를 받아오는지 알려줘야 하는것 같다.
+  controlIncrement = (habit) => {
+    this.props.handleIncrement(habit);
   };
 
-  handleIncrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
-    this.setState({ habits: habits });
+  controlDecrement = (habit) => {
+    this.props.handleDecrement(habit);
   };
 
-  handleDecrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count - 1;
-    habits[index].count = count > 0 ? count : 0;
-    this.setState({ habits });
-  };
-
-  handleDelete = (habit) => {
-    const habits = this.state.habits.filter((item) => item.id !== habit.id);
-    this.setState({ habits });
+  controlDelete = (habit) => {
+    this.props.handleDelete(habit);
   };
 
   render() {
     return (
       <ul>
-        {this.state.habits.map((item) => (
+        {this.props.habits.map((item) => (
           <Habit
             key={item.id}
             habit={item}
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-            onDelete={this.handleDelete}
+            onIncrement={this.controlIncrement}
+            onDecrement={this.controlDecrement}
+            onDelete={this.controlDelete}
           />
         ))}
       </ul>
